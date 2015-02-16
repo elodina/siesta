33bc3de2fac6d0ac3b52f004641eb8827a2d64fb
Ivan Lyutov <ivanlyutov@gmail.com>
2015-02-16 14:36:11 +0200

    Added possible broker errors and error code mapping.

--- errors.go
+++ errors.go
@@ -25,3 +25,57 @@ var InvalidStringLength = errors.New("Invalid string length")
 
 //happens when given value to decode as bytes has either negative or undecodable length
 var InvalidBytesLength = errors.New("Invalid bytes length")
+
+var NoError = errors.New("No error - it worked!")
+
+var Unknown = errors.New("An unexpected server error")
+
+var OffsetOutOfRange = errors.New("The requested offset is outside the range of offsets maintained by the server for the given topic/partition.")
+
+var InvalidMessage = errors.New("Message contents does not match its CRC")
+
+var UnknownTopicOrPartition = errors.New("This request is for a topic or partition that does not exist on this broker.")
+
+var InvalidMessageSize = errors.New("The message has a negative size")
+
+var LeaderNotAvailable = errors.New("In the middle of a leadership election and there is currently no leader for this partition and hence it is unavailable for writes.")
+
+var NotLeaderForPartition = errors.New("You've just attempted to send messages to a replica that is not the leader for some partition. It indicates that the clients metadata is out of date.")
+
+var RequestTimedOut = errors.New("Request exceeds the user-specified time limit in the request.")
+
+var BrokerNotAvailable = errors.New("Broker is likely not alive.")
+
+var ReplicaNotAvailable = errors.New("Replica is expected on a broker, but is not (this can be safely ignored).")
+
+var MessageSizeTooLarge = errors.New("You've just attempted to produce a message of size larger than broker is allowed to accept.")
+
+var StaleControllerEpochCode = errors.New("Broker-to-broker communication fault.")
+
+var OffsetMetadataTooLargeCode = errors.New("You've jsut specified a string larger than configured maximum for offset metadata.")
+
+var OffsetsLoadInProgressCode = errors.New("Offset loading is in progress. (Usually happens after a leader change for that offsets topic partition).")
+
+var ConsumerCoordinatorNotAvailableCode = errors.New("Offsets topic has not yet been created.")
+
+var NotCoordinatorForConsumerCode = errors.New("There is no coordinator for this consumer.")
+
+var BrokerErrors = map[int]error {
+	-1: Unknown,
+	0: NoError,
+	1: OffsetOutOfRange,
+	2: InvalidMessage,
+	3: UnknownTopicOrPartition,
+	4: InvalidMessageSize,
+	5: LeaderNotAvailable,
+	6: NotLeaderForPartition,
+	7: RequestTimedOut,
+	8: BrokerNotAvailable,
+	9: ReplicaNotAvailable,
+	10: MessageSizeTooLarge,
+	11: StaleControllerEpochCode,
+	12: OffsetMetadataTooLargeCode,
+	14: OffsetsLoadInProgressCode,
+	15: ConsumerCoordinatorNotAvailableCode,
+	16: NotCoordinatorForConsumerCode,
+}
