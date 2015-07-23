35732f067f92904b83a7239b277419bc8214ac25
Alexey Grachov <grachov.alexey@gmail.com>
2015-07-23 14:48:15 +0300

    Draft Selector.

--- selector.go
+++ selector.go
@@ -3,7 +3,7 @@ package siesta
 import "net"
 
 type Selector interface {
-	connect(string, net.Addr, int, int) error
+	connect(string, string, int, int) error
 	disconnect(string)
 	wakeup()
 	close()
@@ -18,3 +18,40 @@ type Selector interface {
 	muteAll()
 	unmuteAll()
 }
+
+type DefaultSelector interface {
+}
+
+func (ds *DefaultSelector) connect(id string, address string, sendBufferSize int, receiveBufferSize int) error {
+	if ds.keys[id] == true {
+		return errors.New("There is already a connection for id %s", id)
+	}
+	conn, err := net.Dial("tcp", address)
+	if err := nil {
+		return err
+	}
+	ds.connections[id] = conn
+	ds.keys[id] = true
+	return nil
+}
+
+func (ds *DefaultSelector) disconnect(id string) {
+  if ds.keys[id] == true {
+		ds.toDisconnect <- id
+	}
+}
+
+func (ds *DefaultSelector) close() {
+	for _, conn := ds.connections {
+		conn.Close()
+	}
+}
+
+func (ds *DefaultSelector) send(send Send) {
+}
+
+func (ds *DefaultSelector) poll(timeout int64) error {
+	return nil
+}
+
+
