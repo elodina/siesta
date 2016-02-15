a62c2a38bc6614739b2308099d94a7ec3568dde1
serejja <serejja@gmail.com>
2016-02-15 12:47:33 +0200

    WIP join group

--- join_group.go
+++ join_group.go
@@ -51,3 +51,81 @@ type GroupProtocol struct {
 	ProtocolName     string
 	ProtocolMetadata []byte
 }
+
+type JoinGroupResponse struct {
+	Error         error
+	GenerationID  int32
+	GroupProtocol string
+	LeaderID      string
+	MemberID      string
+	Members       map[string][]byte
+}
+
+func (jgr *JoinGroupResponse) Read(decoder Decoder) *DecodingError {
+	errCode, err := decoder.GetInt16()
+	if err != nil {
+		return NewDecodingError(err, reasonInvalidJoinGroupResponseErrorCode)
+	}
+	jgr.Error = BrokerErrors[errCode]
+
+	generationID, err := decoder.GetInt32()
+	if err != nil {
+		return NewDecodingError(err, reasonInvalidJoinGroupResponseGenerationID)
+	}
+	jgr.GenerationID = generationID
+
+	groupProtocol, err := decoder.GetString()
+	if err != nil {
+		return NewDecodingError(err, reasonInvalidJoinGroupResponseGroupProtocol)
+	}
+	jgr.GroupProtocol = groupProtocol
+
+	leaderID, err := decoder.GetString()
+	if err != nil {
+		return NewDecodingError(err, reasonInvalidJoinGroupResponseLeaderID)
+	}
+	jgr.LeaderID = leaderID
+
+	memberID, err := decoder.GetString()
+	if err != nil {
+		return NewDecodingError(err, reasonInvalidJoinGroupResponseMemberID)
+	}
+	jgr.MemberID = memberID
+
+	membersLength, err := decoder.GetInt32()
+	if err != nil {
+		return NewDecodingError(err, reasonInvalidJoinGroupResponseMembersLength)
+	}
+
+	if membersLength == 0 {
+		return nil
+	}
+
+	jgr.Members = make(map[string][]byte, membersLength)
+	for i := 0; i < membersLength; i++ {
+		memberID, err := decoder.GetString()
+		if err != nil {
+			return NewDecodingError(err, reasonInvalidJoinGroupResponseMembersMemberID)
+		}
+
+		memberMetadata, err := decoder.GetBytes()
+		if err != nil {
+			return NewDecodingError(err, reasonInvalidJoinGroupResponseMembersMemberMetadata)
+		}
+
+		jgr.Members[memberID] = memberMetadata
+	}
+
+	return nil
+}
+
+var (
+	reasonInvalidJoinGroupResponseErrorCode             = "Invalid error code in JoinGroupResponse"
+	reasonInvalidJoinGroupResponseGenerationID          = "Invalid generation id in JoinGroupResponse"
+	reasonInvalidJoinGroupResponseGroupProtocol         = "Invalid group protocol in JoinGroupResponse"
+	reasonInvalidJoinGroupResponseLeaderID              = "Invalid leader id in JoinGroupResponse"
+	reasonInvalidJoinGroupResponseMemberID              = "Invalid member id in JoinGroupResponse"
+	reasonInvalidJoinGroupResponseMembersLength         = "Invalid members length in JoinGroupResponse"
+	reasonInvalidJoinGroupResponseMembersMemberID       = "Invalid member id in members array in JoinGroupResponse"
+	reasonInvalidJoinGroupResponseMembersMemberMetadata = "Invalid member metadata in members array in JoinGroupResponse"
+)
